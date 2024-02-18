aa21bd22b052c6070ba7a476b7287ce5479896fa
Saqib <saqibnawab823@gmail.com>
2024-02-18 12:20:21 +0500

    added authentication mechanism in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +8,28 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+// Set up session middleware with a secret for encryption
+app.use(session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Authentication mechanism using session authorization
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Check if the user is authenticated
+    if (req.session && req.session.user) {
+        // If user is authenticated, move to the next middleware
+        next();
+    } else {
+        // If user is not authenticated, return an error response
+        return res.status(401).json({ message: "Unauthorized" });
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
